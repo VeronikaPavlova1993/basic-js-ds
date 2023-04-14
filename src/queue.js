b74c39b32bc970981d1d551a4fb36b1095021b96
Your Name <you@example.com>
2023-04-14 19:13:36 +0400

    fead: add task queue

--- src/queue.js
+++ src/queue.js
@@ -1,62 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+* Implement the Queue with a given interface via linked list (use ListNode extension above).
+*
+* @example
+* const queue = new Queue();
+*
+* queue.enqueue(1); // adds the element to the queue
+* queue.enqueue(3); // adds the element to the queue
+* queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+* queue.getUnderlyingList() // returns { value: 3, next: null }
+*/
 class Queue {
-  constructor() {
-    this.top = null;
-    this.end = null;
-    this.length = 0;
-  }
-
-  getUnderlyingList() {
-    let lists = '';
-   let list = this.top;
-   while(list) {
-   lists += `{"value":${list.value},"next": ${list.next}}`;
-    list = list.next;
-   }
-   return lists;
-  }
-
-
-  enqueue(value) {
-   const node = new Node(value);
-   if (this.top) {
-    this.end.next = node;
-    this.end = node;
-   } else{
-    this.top = node;
-    this.end = node;
-   }
-   this.length++;
-   }
-
-  dequeue() {
-    const removal = this.top;
-    this.top = this.top.next;
-    this.length--;
-    return removal.value;
-  }
+constructor() {
+this.head = null;
+this.tail = null;
+this.length = 0;
 }
-let queue = new Queue();
+
+getUnderlyingList() {
+return this.head;
+}
+
+enqueue(value) {
+const node = new ListNode(value);
+
+if (this.head) {
+this.tail.next = node;
+this.tail = node;
+} else {
+this.head = node;
+this.tail = node;
+}
+
+this.length++;
+}
+
+dequeue() {
+const current = this.head;
+this.head = this.head.next;
+this.length--;
+return current.value;
+}
+}
+
 module.exports = {
-  Queue
+Queue,
 };
